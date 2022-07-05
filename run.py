62ba420f2889d704180a7e24504a4013936360b3
Zofia <z.przybyl11@gmail.com>
2022-07-05 15:37:54 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+
+from keywords import(
+    YES,
+    NO
+)
+
+def print_text(text: str, delay: int):
+    '''
+    Function prints text after user input
+    '''
+    print(text)
+    time.sleep(delay)
+
+def begin_game():
+    print("Welcome to the journey. Would you like to begin? (yes/no)")
+    response = input("")
+    while response not in [YES, NO]:
+        print_text("Please enter a valid response", 2)
+        response = input("")
+    if response in YES:
+        print_text("Let's begin!", 2)
+
+
+
+
+
