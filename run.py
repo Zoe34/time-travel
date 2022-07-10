a26323eb25ec88d26e3c2291be5a39838e1fb253
Zofia <z.przybyl11@gmail.com>
2022-07-10 23:24:45 +0000

    Create option to open or ignore the door and spin up/down option

--- run.py
+++ run.py
@@ -6,7 +6,11 @@ from keywords import (
     OPEN,
     CARRY_ON,
     BLUE,
-    RED
+    RED,
+    ANSWER,
+    IGNORE,
+    UP,
+    DOWN
 )
 
 
@@ -20,14 +24,14 @@ def print_text(text: str, delay: int):
 
 def start_again():
     '''
-    Game begins again when the user decides to quit once asked "Are you ready?" 
+    Game begins again when the user decides to quit once asked "Are you ready?"
     '''
     begin_game()
 
 
 def begin_game():
     '''
-    This function runs the game and all the main questions for the user are 
+    This function runs the game and all the main questions for the user are
     placed here.
     '''
     print("Welcome to the journey. Would you like to begin? (yes/no)")
@@ -55,18 +59,16 @@ def begin_game():
         response = input("")
 
     if response in YES:
-        print_text("Great. You are currently in your family home.", 1)
+        print_text("Great. You are currently in your family home.", 2)
         print_text("However not the one you know of.", 2)
         print_text("The interior looks different.", 2)
         print_text("You scan the room and see several unfamiliar items,", 2)
         print_text("such as various paintings on vintage, floral wallpaper", 2)
         print_text("Your hear a noise in the room next door.", 2)
         print_text("A banging and sounds of moved items across a surface", 2)
-    
     elif response in NO:
         print_text("Goodbye, thanks for coming.", 2)
         start_again()
-        
     print("Do you want to open the door or carry on?\n")
     print("open/carry on\n")
     response = input("")
@@ -105,13 +107,10 @@ def begin_game():
         blue_or_red()
 
     elif response in CARRY_ON:
-        print_text("You walk downstairs", 2)
-        print_text("You walk towards what seems to be the kitchen", 2)
-        print_text("and open the door", 2)
-        print_text("The kitchen looks exatly like the room upstairs", 2)
-        print_text("In fact it is the same room", 2)
-        print_text("It's as though you never left that room...", 2)
-        
+        print_text("You walk along the corridor", 2)
+        print_text("and you suddenly hear knocking at the front door.", 2)
+        open_or_not()
+
 
 def blue_or_red():
     '''
@@ -125,7 +124,9 @@ def blue_or_red():
     if response in RED:
         print(f"You picked {response}")
         print_text("You have been sucked into a black hole.", 2)
+        print_text("", 1)
         print_text("The time capsule couldn't take the pressure", 2)
+        print_text("", 1)
         print_text("Game over.", 2)
         start_again()
     elif response in BLUE:
@@ -142,4 +143,87 @@ def blue_or_red():
         start_again()
 
 
+def open_or_not():
+    '''
+    The user can decide whether to open the door or not in the game.
+    This function uses the "answer" and "ignore" keywords.
+    '''
+    print("Will you open the door? (answer/ignore)")
+    response = input("")
+
+    while response not in [ANSWER, IGNORE]:
+        print_text("Response is invalid, please try again", 2)
+        response = input("")
+    if response in ANSWER:
+        print_text("You walk down the stairs and towards the front door", 2)
+        print_text("You open the door and see no one.", 2)
+        print_text("Instead of a person you spot a parcel", 2)
+        print_text("on the ground.", 2)
+        print_text("You see your name written in beautiful,", 2)
+        print_text("cursive handwriting on the brown wrapping paper.", 2)
+        print_text("Upon opening the package you realise", 2)
+        print_text("it held a watch. It has a golden crown on the side", 2)
+        spin_up_or_down()
+    elif response in IGNORE:
+        print_text("You head to the garden", 2)
+        print_text("and spot a large shed in the corner", 2)
+        print_text("When entering you notice a huge metal construction", 2)
+        print_text("You also spot it has an entrance", 2)
+        print_text("You step inside and find two different", 2)
+        print_text("coloured levers", 2)
+        blue_or_red()
+
+
+def spin_up_or_down():
+    '''
+    This function operates through the "up" and "down" keywords.
+    '''
+    print("Do you want to spin the crown up or down?")
+    response = input("")
+
+    while response not in [UP, DOWN]:
+        print_text("Response is invalid, please try again", 2)
+        response = input("")
+    if response in UP:
+        print_text("You spin upwards.", 2)
+        print_text("Your surroundings become blurred.", 2)
+        print_text("You start to sway and realise", 2)
+        print_text("you can no longer hold yourself up.", 2)
+        print_text("Your vision goes fully black.", 2)
+        print_text("...", 2)
+        print_text("", 1)
+        print_text("", 1)
+        print_text("When you open your eyes you recognise your home;", 2)
+        print_text("However when you look down at the watch", 2)
+        print_text("it is smashed, rusty and looks nothing like", 2)
+        print_text("it did in the 1920s from which you came.", 2)
+        print_text("You made it back, congratulations", 2)
+        start_again()
+    elif response in DOWN:
+        print_text("You spin upwards.", 2)
+        print_text("Your surroundings become blurred.", 2)
+        print_text("You start to sway and realise", 2)
+        print_text("you can no longer hold yourself up.", 2)
+        print_text("Your vision turns fully black.", 2)
+        print_text("...", 2)
+        print_text("", 1)
+        print_text("", 1)
+        print_text("When you open your eyes you are still lying down", 2)
+        print_text("in the hallway", 2)
+        print_text("The bright green wallpaper draws your attention", 2)
+        print_text("as it's different from the previous 1920 decor", 2)
+        print_text("You check the newspaper on top of the", 2)
+        print_text("large table on the side.", 2)
+        print_text("23.06.1962", 2)
+
+
+def main():
+    '''
+    This function runs the game and all the other functions
+    '''
+
+
 begin_game()
+
+if __name__ == '__main__':
+    main()
